e2af35d5c6522a77071fa70ce82833dedadb7492
JuanDEV <juan_aguilar1290@outlook.es>
2022-10-07 22:49:45 -0500

    Model for students were added

---
+++ models/students.js
@@ -0,0 +1,24 @@
+const { DataTypes, Sequelize } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Student = sequelize.define('Student', {
+    name: {
+        type: DataTypes.CHAR(60),
+        allowNull: false
+    },
+    surname: {
+        type: DataTypes.CHAR(60),
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.CHAR(64),
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    }
+
+});
+
+module.exports = Student;
