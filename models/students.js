30ec659a5ffd84a438b6735f3b4237733ebe90a2
JuanDEV <juan_aguilar1290@outlook.es>
2022-10-07 22:59:22 -0500

    Sequelize const deleted

--- models/students.js
+++ models/students.js
@@ -1,4 +1,4 @@
-const { DataTypes, Sequelize } = require('sequelize');
+const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
 const Student = sequelize.define('Student', {
