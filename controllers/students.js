1f094c5a2662260f5653d3b249ccc1baf4d72607
JuanDEV <juan_aguilar1290@outlook.es>
2022-10-07 22:50:23 -0500

    Controller for students were added

---
+++ controllers/students.js
@@ -0,0 +1,43 @@
+const Student = require('../models/students');
+
+async function createStudent(req, res){
+    const body = req.body;
+    const student = Student.create(body);
+    res.status(201).json(student);
+}
+
+async function getStudent(req, res){
+    const id = req.params.id;
+    const student = Student.findByPk(id);
+    res.status(200).json(student);
+} 
+
+async function getStudents(req, res){
+    const students = Student.findAll();
+    res.status(200).json(students);
+}
+
+async function updateStudent(req, res) {
+    const id = req.params.id;
+    const student = req.body;
+    await Student.update(student, {where: {id}});
+    const student_updated = await God.findByPk(id);
+    res.status(200).json(student_updated);
+}
+
+async function deleteStudent(req, res) {
+    const id = req.params.id;
+    const deleted = Student.destroy(
+        {where: {id} }
+    );
+    res.status(200).json(deleted);
+}
+
+
+module.exports = {
+    createStudent, 
+    getStudent,
+    getStudents,
+    updateStudent,
+    deleteStudent
+}
